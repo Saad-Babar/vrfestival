1fa18ba6c95408d6d71e33d9f6fae943bbaeee78
Saad-Babar <saadmubeen2004@gmail.com>
2023-11-08 16:41:58 +0500

    halalala

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,29 +2,49 @@
 
 // images slider 
 
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel');
+
+owl.owlCarousel({
   loop: true,
   margin: 10,
   autoplay: true,
   responsiveClass: true,
   responsive: {
-      0: {
-          items: 1,
-          nav: true,
-          loop: true,
-      },
-      600: {
-          items: 1,
-          nav: false,
-          loop: true,
-      },
-      1000: {
-          items: 3.5,
-          nav: true,
-          loop: true,
-      }
+    0: {
+      items: 1,
+    },
+    600: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    1000: {
+      items: 3.5,
+    },
+    1024: {
+      items:2.5,
+    },
+    1366: {
+      items: 3,
+    },
+    2560: {
+      items: 5.5,
+    }
+
   }
 });
+
+// Pause the carousel on mouseenter
+owl.on('mouseenter', function() {
+  owl.trigger('stop.owl.autoplay');
+});
+
+// Resume the carousel on mouseleave
+owl.on('mouseleave', function() {
+  owl.trigger('play.owl.autoplay');
+});
+
 
 
 // sidenav 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,35 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      screens: {
+        // coxs: '375px',
+        // xs: '425px',
+        // cosm: '500px',
+        // sm: '576px',
+        // comd: '680px',
+        // colg: '850px',
+        // lg: '992px',
+        // md: '768px',
+        xl: '1024px',
+        coxl: '1366px',
+        '2xl': '1440px',
+        max: '2560px',
+        // max-width breakpoints
+        // maxcosm: { max: '424.5px' },
+        md: { max: '768px' },
+        maxcomd: { max: '425px' },
+        // maxmd: { max: '767.5px' },
+        // maxcolg: { max: '849.5px' },
+        // maxlg: { max: '991.5px' },
+        // maxxl: { max: '1199.5px' },
+        // min and max width breakpoints
+        // 'md-to-lg': { min: '768px', max: '992px' },
+        // 'lg-to-xl': { min: '992px', max: '1199px' },
+      },
       colors: {
+        blck: '#000000',
+        gren: '#A3D5BD',
+        wite: '#FFFFFF',
         c1: '#F5F5F5',
         c2: '#F4F4F4',
         c3: '#3D3D3D',
